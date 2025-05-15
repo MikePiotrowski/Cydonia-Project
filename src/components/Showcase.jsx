c61ca9d450c700172444f103742148183e0fbc82
Michael Lee Piotrowski <mlp95747@gmail.com>
2025-05-15 07:54:05 -0700

    Theme improvements

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Showcase.css';
 
 const Showcase = () => {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const images = [
+        '/img/showcase.jpg',
+        '/img/Mars.jpg'
+    ];
+
+    useEffect(() => {
+        // Change background image every 5 seconds
+        const interval = setInterval(() => {
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 5000);
+
+        return () => clearInterval(interval); // Clean up on unmount
+    }, []);
+
   return (
-    <section id="showcase" role="region" aria-labelledby="showcase-heading">
+      <section
+          id="showcase"
+          role="region"
+          aria-labelledby="showcase-heading"
+          style={{
+              backgroundImage: `url(${images[currentImageIndex]})`,
+              transition: 'background-image 1s ease-in-out'
+          }}
+      >
       <div className="container">
         <h1 id="showcase-heading">The Mysteries of Cydonia on Mars</h1>
         <p>
